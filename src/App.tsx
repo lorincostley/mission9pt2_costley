4c74182bec752038b9b471fec7ac0cafa28d2ada
lorincostley <lorin.costley@gmail.com>
2024-03-08 11:52:50 -0700

    yeah buddy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import teamsData from './CollegeBasketballTeam.json';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>NCAA Basketball Teams</h1>;
+}
+
+function TeamCard(props: TeamProps) {
+  return (
+    <div className="team-card-box">
+      <div className="team-card">
+        <h2>{props.school}</h2>
+        <h3>Mascot: {props.name}</h3>
+        <h3>City: {props.city}</h3>
+        <h3>State: {props.state}</h3>
+      </div>
+    </div>
+  );
+}
+
+function TeamList() {
+  return (
+    <div className="team-list">
+      {teamsData.teams.map((team: TeamProps, index: number) => (
+        <TeamCard key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
